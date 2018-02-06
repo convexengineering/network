ff24fbd971224ea91a9e9efb58db8809a0c25fab
1ozturkbe <1ozturkbe@gmail.com>
2018-02-06 13:44:10 -0500

    Working prototype of networkDiagram.

--- network.py
+++ network.py
@@ -50,11 +50,7 @@ class Node(Model):
 	def setup(self,edgeList):
 		flow = VectorVariable('')
 
-if __name__ == '__main__':
-	m = basicFlow()
-	m = Model(m.variables_byname('totalCost')[0],Bounded(m))
-	sol = m.localsolve(verbosity=4)
-
+def drawNetwork(sol):
 	#Visualize the flow
 	g = nx.Graph()
 	flow = sol('flow')
@@ -63,23 +59,43 @@ if __name__ == '__main__':
 	nodeNames = ['a','b','c','d','e']
 	g.add_nodes_from(nodeNames)
 
-	edge_alphas = np.zeros((10,1))
-	edge_weights = np.zeros((10,1))
+
+
+if __name__ == '__main__':
+	m = basicFlow()
+	m = Model(m.variables_byname('totalCost')[0],Bounded(m))
+	sol = m.localsolve(verbosity=4)
+
+	g = nx.Graph()
+	flow = sol('flow')
+	sources = sol('source')
+	sinks = sol('sink')
+	nodeNames = ['a','b','c','d','e']
+	g.add_nodes_from(nodeNames)
+
+
+	edge_alphas = np.zeros(10)
+	edge_weights = np.zeros(10)
 
 	count = 0
 	for i in range(0,5):
 		for j in range(0,5):
 			if j > i:
-				edge_weights[count] = abs(flow[i,j]-flow[j,i])
-				g.add_edge(nodeNames[i],nodeNames[j],weight=10*edge_weights[count,0])
+				edge_weights[count] = flow[i,j]-flow[j,i]
+				if edge_weights[count] > 0:
+					g.add_edge(nodeNames[i],nodeNames[j],weight=abs(edge_weights[count]))
+				else:
+					g.add_edge(nodeNames[j],nodeNames[i],weight=abs(edge_weights[count]))
 				count += 1
 
 	pos = nx.shell_layout(g)
 
-	node_sizes = abs(sources - sinks)
+	node_sizes = sources - sinks
+	node_colors = ['r' if i < 0 else 'b' for i in node_sizes]
+
 	edge_alphas = edge_weights/max(edge_weights)
-	nodes = nx.draw_networkx_nodes(g,pos,nodesize=node_sizes,nodelist=nodeNames,node_color='b')
-	edges = nx.draw_networkx_edges(g, pos,nodesize=node_sizes,arrowstyle='->',arrowsize=10,width=2,edgecolor='b')
+	nodes = nx.draw_networkx_nodes(g,pos,nodesize=abs(node_sizes),nodelist=nodeNames,node_color=node_colors)
+	edges = nx.draw_networkx_edges(g, pos,nodesize=abs(node_sizes),arrowstyle='->',arrowsize=10,width=abs(edge_weights),edgecolor='b')
 
 	m = g.number_of_edges()
 
