3c1cbc1e1f9fec7bc62eee7dfd6ed135b429358d
1ozturkbe <1ozturkbe@gmail.com>
2018-02-20 11:32:47 -0500

    Some clean-up in LP solver.

--- solveNetworkLP.py
+++ solveNetworkLP.py
@@ -8,6 +8,14 @@ def flatten(A):
             flat_list.append(item)
     return flat_list
 
+def square(vec):
+    N = int(len(vec)**0.5)
+    square_list = np.zeros([N,N])
+    for i in range(N):
+        for j in range(N):
+            square_list[i,j] = vec[i+N*j]
+    return square_list
+
 def solveNetworkLP(N,edgeCosts,edgeMaxFlows,sources,sinks):
     c = flatten(edgeCosts)
     A_ub = None
@@ -22,22 +30,14 @@ def solveNetworkLP(N,edgeCosts,edgeMaxFlows,sources,sinks):
             Adum[i,i] = 0
         A_eq[i,:] = flatten(Adum)
 
-    b_eq = np.zeros(N)
     b_eq = np.array(sources) - np.array(sinks)
-
-    bounds = np.zeros([N,N])
     bounds = [(0,j) for sublist in edgeMaxFlows for j in sublist]
 
     solution = scipy.optimize.linprog(c, A_ub, b_ub, A_eq, b_eq, bounds)
 
     # Giving same dict output as GP solve
-    flow = np.zeros([N,N])
-    for i in range(N):
-        for j in range(N):
-            flow[i,j] = solution['x'][i+N*j]
-
+    flow = square(solution['x'])
     solDict = {}
     solDict['flow'] = flow
-
     return solDict
 
