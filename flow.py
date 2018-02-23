1bc2440a88fa645de73b170b474e257fa208b4a4
1ozturkbe <1ozturkbe@gmail.com>
2018-02-22 20:23:00 -0500

    First quasi-working algorithm wooohooo!

--- flow.py
+++ flow.py
@@ -11,6 +11,7 @@ class Flow(Model):
         flow = VectorVariable([N, N], 'flow')
         source = VectorVariable(N, 'source')
         sink = VectorVariable(N, 'sink')
+        slack = VectorVariable(N, 'slack')
         totalCost = Variable('totalCost')
 
         constraints = []
@@ -18,12 +19,13 @@ class Flow(Model):
         with SignomialsEnabled():
 
             for i in range(0, N):
-                constraints.extend([sink[i] + sum(flow[i, :]) <= source[i] + sum(flow[:, i])])
+                constraints.extend([sink[i] + sum(flow[i, :]) <= slack[i]*(source[i] + sum(flow[:, i])),
+                                    slack[i] >= 1])
                 for j in range(0, N):
                     constraints += [flow[i, j] <= connect[i,j]*edgeMaxFlow[i, j],
-                                    connect[i,j] <= 1.]
+                                    connect[i,j] <= 1.,]
             for i in range(0, N):
                 for j in range(i + 1, N):
                     constraints.extend([connect[i, j] * connect[j, i] <= 1e-20])
-        constraints.extend([totalCost >= np.sum(edgeCost * flow) + 10*np.sum(edgeCost * connect)])
+        constraints.extend([totalCost >= np.sum(edgeCost * flow) + 1e4*np.prod(slack)])
         return constraints
--- solveNetworkGP.py
+++ solveNetworkGP.py
@@ -1,7 +1,7 @@
 from gpkit import Variable, VectorVariable, Model, SignomialsEnabled, SignomialEquality
 from gpkit.constraints.bounded import Bounded
 from flow import Flow
-
+from relaxed_constants import relaxed_constants
 def solveNetworkGP(N,edgeCosts,edgeMaxFlows,sources,sinks):
     m = Flow(N)
     m.substitutions.update({
@@ -11,5 +11,6 @@ def solveNetworkGP(N,edgeCosts,edgeMaxFlows,sources,sinks):
         'sink'       :    sinks,
         })
     m = Model(m.variables_byname('totalCost')[0], Bounded(m))
+    #m = relaxed_constants(m)
     sol = m.localsolve(verbosity=4, reltol=10**-5,iteration_limit=100)
     return sol
